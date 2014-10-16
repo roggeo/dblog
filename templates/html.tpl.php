560606f4835e1e34350c95ccd089d48dcb17b0df
Geovani <geovanirog@gmail.com>
2014-10-16 09:45:32 -0300

    Deploy Dblog Theme Drupal
    first version of the file

---
+++ templates/html.tpl.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+<head>
+<?php print $head; ?>
+<meta name="Author" content="Dblog">
+<title><?php print $head_title; ?></title>
+<?php print $styles; ?>
+<?php print $scripts; ?>
+<!--[if lt IE 9]><script src="<?php print base_path() . drupal_get_path('theme', 'dblog') . '/js/html5.js'; ?>"></script><![endif]-->
+</head>
+<body class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
